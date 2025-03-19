97b769c3bc3b531142f80d6a8f6d34ca292a3059
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-03-19 20:38:52 +0300

    initial commit

---
+++ tets_simple/test_pass.py
@@ -0,0 +1,33 @@
+
+
+
+def test_pass1():
+    pass
+
+
+def test_pass2():
+    pass
+
+
+def test_pass3():
+    pass
+
+
+def test_pass4():
+    pass
+
+
+def test_pass5():
+    pass
+
+
+def test_pass6():
+    pass
+
+
+def test_pass7():
+    pass
+
+
+def test_pass8():
+    pass
